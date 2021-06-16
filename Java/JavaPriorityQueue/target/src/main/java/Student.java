d08e839a17b046ab387e8ea99db2d9aea0f97f30
devagx <62251824+devagx@users.noreply.github.com>
2021-06-16 18:31:12 +0100

    Add files via upload

---
+++ Java/JavaPriorityQueue/target/src/main/java/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private final int id;
+    private final String name;
+    private final double cgpa;
+
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getCGPA() {
+        return this.cgpa;
+    }
+}
