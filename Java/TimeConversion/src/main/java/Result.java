d1f3f7b1f639111d1973538ab6ebc309c5db00ab
Alun Groome <alungroome@hotmail.com>
2023-01-17 17:31:16 +0000

    Refactor

--- Java/TimeConversion/src/main/java/Result.java
+++ Java/TimeConversion/src/main/java/Result.java
@@ -19,4 +19,29 @@ public class Result {
 
         return output;
     }
+
+
+    //Alternative solution
+
+//    public static String timeConversion(String s) throws ParseException {
+//        String time = s;
+//        String listTime[] = time.split(":");
+//        String hour = listTime[0];
+//        String minutes = listTime[1];
+//        String secounds = listTime[2].substring(0, 2);
+//        String caser = listTime[2].substring(2, 4);
+//        if(caser.equals("AM")){
+//            if(hour.equals("12"))
+//                hour="00";
+//
+//            return hour+":"+minutes+":"+secounds;
+//        }else{
+//            if(!hour.equals("12")){
+//                int h = Integer.parseInt(hour);
+//                h = h + 12;
+//                hour =""+h;
+//            }
+//            return hour+":"+minutes+":"+secounds;
+//        }
+//    }
 }
