d946dc0dbb8de79ef4e8f8a330c3e538d8d14077
Alun Groome <alungroome@hotmail.com>
2023-01-17 11:11:41 +0000

    Added base project

---
+++ Java/MinMaxSum/src/test/java/MinMaxSumTests.java
@@ -0,0 +1,7 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class MinMaxSumTests {
+}
