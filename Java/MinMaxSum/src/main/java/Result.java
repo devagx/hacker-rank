d6286b280263e71f4c79b2ce19d0ae3a2032642e
Alun Groome <alungroome@hotmail.com>
2023-01-17 14:51:02 +0000

    Refactors

--- Java/MinMaxSum/src/main/java/Result.java
+++ Java/MinMaxSum/src/main/java/Result.java
@@ -23,38 +23,3 @@ public class Result {
         System.out.println(minSum + " " + maxSum);
     }
 }
-
-/*
-
-CLIENT
-
-INPUT A single line of five space-separated integers.
-
-
-FUNCTION
-
-INPUT: arr: an array of  integers
-OUTPUT: 2 LONG integers (Minsum max sum space seperated)
-
-LOGIC
-
-Loop around array
-    variable int min number Index
-    variable int max number Index
-
-    variable long sum min
-    variable long sum max
-
-    each itteration check if current number is smaller than min number, if so then set min number index to  current index
-    each itteration check if current number is bigger than max number, if so then set max number index to  current index
-
-OUTSIDE LOOP
-long sum min = sum of integers except min number Index
-long sum max = sum of integers except max number Index
-
-PRINT
-     variable long min number
-    variable long max number
-
-
- */
