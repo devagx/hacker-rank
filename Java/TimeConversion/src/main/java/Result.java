761e6fc5dc4c92d9c52e8c2d8264895f491ef07c
Alun Groome <alungroome@hotmail.com>
2023-01-17 17:34:58 +0000

    Refactor

--- Java/TimeConversion/src/main/java/Result.java
+++ Java/TimeConversion/src/main/java/Result.java
@@ -7,29 +7,41 @@ public class Result {
     private Result() {
     }
 
+    public static String timeConversion(String s) throws ParseException {
+        Date date = null;
+        String output = null;
+
+        DateFormat inputFormat = new SimpleDateFormat("hh:mm:ssaa");
+        DateFormat outputFormat = new SimpleDateFormat("HH:mm:ss");
+
+        date = inputFormat.parse(s);
+        output = outputFormat.format(date);
+
+        return output;
+    }
 
 
     //Alternative solution
 
-    public static String timeConversion(String s) throws ParseException {
-        String time = s;
-        String listTime[] = time.split(":");
-        String hour = listTime[0];
-        String minutes = listTime[1];
-        String secounds = listTime[2].substring(0, 2);
-        String caser = listTime[2].substring(2, 4);
-        if(caser.equals("AM")){
-            if(hour.equals("12"))
-                hour="00";
-
-            return hour+":"+minutes+":"+secounds;
-        }else{
-            if(!hour.equals("12")){
-                int h = Integer.parseInt(hour);
-                h = h + 12;
-                hour = String.valueOf(h);
-            }
-            return hour+":"+minutes+":"+secounds;
-        }
-    }
+//    public static String timeConversion(String s) throws ParseException {
+//        String time = s;
+//        String listTime[] = time.split(":");
+//        String hour = listTime[0];
+//        String minutes = listTime[1];
+//        String secounds = listTime[2].substring(0, 2);
+//        String caser = listTime[2].substring(2, 4);
+//        if(caser.equals("AM")){
+//            if(hour.equals("12"))
+//                hour="00";
+//
+//            return hour+":"+minutes+":"+secounds;
+//        }else{
+//            if(!hour.equals("12")){
+//                int h = Integer.parseInt(hour);
+//                h = h + 12;
+//                hour = String.valueOf(h);
+//            }
+//            return hour+":"+minutes+":"+secounds;
+//        }
+//    }
 }
