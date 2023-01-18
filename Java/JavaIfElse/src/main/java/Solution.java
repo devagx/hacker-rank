8688ba4c30f1ae285220a42c16b7bdd46222f15e
Alun Groome <alungroome@hotmail.com>
2023-01-18 11:55:49 +0000

    Refactors

---
+++ Java/JavaIfElse/src/main/java/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        System.out.println(JavaIfElse.getAnswer(N));
+    }
+}
+
