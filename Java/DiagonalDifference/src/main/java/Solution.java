f42b8268ae498d2a3831797f21001f951cb15186
Alun Groome <alungroome@hotmail.com>
2023-01-25 13:30:30 +0000

    Created base project.

---
+++ Java/DiagonalDifference/src/main/java/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+//        int result = Result.diagonalDifference(arr);
+//
+//        System.out.println(String.valueOf(result));
+
+        bufferedReader.close();
+    }
+}
