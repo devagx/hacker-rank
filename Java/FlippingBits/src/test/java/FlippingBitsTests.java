ecd43d1dc54ec8179ec5cb5f0beefa0ea1c6e143
Alun Groome <alungroome@hotmail.com>
2023-01-25 13:25:25 +0000

    Refactors

--- Java/FlippingBits/src/test/java/FlippingBitsTests.java
+++ Java/FlippingBits/src/test/java/FlippingBitsTests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,6 +13,18 @@ class FlippingBitsTests {
     })
     void flippingBits_4294967295MinusInput_returnsOutput(long input, long output, long baseValue) {
         long result = baseValue - input;
+
+        assertThat(result).isEqualTo(output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2147483647, 2147483648",
+            "1, 4294967294",
+            "0, 4294967295",
+    })
+    void flippingBits_4294967295MinusInputUsingFunction_returnsOutput(long input, long output) {
+        long result = Result.flippingBits(input);
 
         assertThat(result).isEqualTo(output);
     }
