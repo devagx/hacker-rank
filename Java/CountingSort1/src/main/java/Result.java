15f284a6d02a32ea3d7d2fea41bef0be061cad48
Alun Groome <alungroome@hotmail.com>
2023-01-26 15:19:38 +0000

    Created tests and solution

---
+++ Java/CountingSort1/src/main/java/Result.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Result {
+
+    private Result() {
+
+    }
+
+    public static List<Integer> countingSort(List<Integer> inputArray) {
+        Collections.sort(inputArray);
+
+        List<Integer> outputArray = new ArrayList<>();
+
+        int o = 0;
+
+        while (o < 100) {
+            outputArray.add(0);
+            o++;
+        }
+
+        inputArray.forEach(val -> {
+            outputArray.set(val, outputArray.get(val) + 1);
+        });
+
+        return outputArray;
+    }
+}
+
+/*
+--  Sort input array inc
+-- create outputArray with size of last position of input array
+
+--LOOP INPUT ARRAY
+    -set value of output array at position i with value of input array at position i
+ */
--- Java/CountingSort1/src/test/java/CountingSort1Tests.java
+++ Java/CountingSort1/src/test/java/CountingSort1Tests.java
@@ -1,5 +1,131 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 class CountingSort1Tests {
+
+    @Test
+    void countingSort_singleValueArray_returnsSingleArrayWithValueSetInCorrectPositionRestAreZero() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(2);
+
+        List<Integer> expectedOutputArray = new ArrayList<>();
+
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(1);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).element(0).isEqualTo(0);
+        assertThat(resultArray).element(1).isEqualTo(0);
+        assertThat(resultArray).element(2).isEqualTo(1);
+    }
+
+    @Test
+    void countingSort_doubleValueArrayWithDuplicates_returnsSingleArrayWithSingleValueIncrementedRestAreZero() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(3);
+        inputArray.add(3);
+
+        List<Integer> expectedOutputArray = new ArrayList<>();
+
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(2);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).element(0).isEqualTo(0);
+        assertThat(resultArray).element(1).isEqualTo(0);
+        assertThat(resultArray).element(2).isEqualTo(0);
+        assertThat(resultArray).element(3).isEqualTo(2);
+    }
+
+    @Test
+    void countingSort_doubleValueArrayWithNoDuplicates_returnsSingleArrayWith2ValuesSetRestAreZero() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(1);
+        inputArray.add(3);
+
+        List<Integer> expectedOutputArray = new ArrayList<>();
+
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(1);
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(1);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).element(0).isEqualTo(0);
+        assertThat(resultArray).element(1).isEqualTo(1);
+        assertThat(resultArray).element(2).isEqualTo(0);
+        assertThat(resultArray).element(3).isEqualTo(1);
+    }
+
+    @Test
+    void countingSort_tripleValueArrayWithDuplicatesAndNoDuplicates_returnsSingleArray1ValueSetNotIncrementedAnd1ValueSetIncrementedRestAreZero() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(1);
+        inputArray.add(1);
+        inputArray.add(3);
+
+        List<Integer> expectedOutputArray = new ArrayList<>();
+
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(2);
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(1);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).element(0).isEqualTo(0);
+        assertThat(resultArray).element(1).isEqualTo(2);
+        assertThat(resultArray).element(2).isEqualTo(0);
+        assertThat(resultArray).element(3).isEqualTo(1);
+    }
+
+    @Test
+    void countingSort_anyInputArray_resultArrayHasSize100() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(2);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).hasSize(100);
+    }
+
+    @Test
+    void countingSort_arrayWithMixedValues_returnsSingleArrayWithCorrectValuesSet() {
+        List<Integer> inputArray = new ArrayList<>();
+
+        inputArray.add(1);
+        inputArray.add(1);
+        inputArray.add(3);
+        inputArray.add(2);
+        inputArray.add(1);
+
+        List<Integer> expectedOutputArray = new ArrayList<>();
+
+        expectedOutputArray.add(0);
+        expectedOutputArray.add(3);
+        expectedOutputArray.add(1);
+        expectedOutputArray.add(1);
+
+        List<Integer> resultArray = Result.countingSort(inputArray);
+
+        assertThat(resultArray).element(0).isEqualTo(0);
+        assertThat(resultArray).element(1).isEqualTo(3);
+        assertThat(resultArray).element(2).isEqualTo(1);
+        assertThat(resultArray).element(3).isEqualTo(1);
+    }
 }
