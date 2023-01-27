08717d7e028a77c23ba6192655363fdf92b150f0
Alun Groome <alungroome@hotmail.com>
2023-01-27 20:05:25 +0000

    Created base project SubarrayDivision1

---
+++ Java/SubarrayDivision1/src/main/java/Solution.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+//        int result = Result.birthday(s, d, m);
+//
+//        System.out.println(String.valueOf(result));
+
+        bufferedReader.close();
+    }
+}
