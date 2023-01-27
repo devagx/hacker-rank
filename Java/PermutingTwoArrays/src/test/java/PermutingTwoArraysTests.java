9c9145229052b650ccdb848df54408df8053697c
Alun Groome <alungroome@hotmail.com>
2023-01-27 19:41:34 +0000

    Added tests

--- Java/PermutingTwoArrays/src/test/java/PermutingTwoArraysTests.java
+++ Java/PermutingTwoArrays/src/test/java/PermutingTwoArraysTests.java
@@ -1,15 +1,47 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 class PermutingTwoArraysTests {
+    @Test
+    void twoArrays_validPermutation_returnsYes() {
+        int k = 1;
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        a.add(0);
+        a.add(1);
+
+        b.add(0);
+        b.add(2);
+
+        String result = Result.twoArrays(k, a, b);
+
+        assertThat(result).isEqualTo("YES");
+    }
+
+    @Test
+    void twoArrays_invalidPermutation_returnsNo() {
+        int k = 5;
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        a.add(1);
+        a.add(2);
+        a.add(2);
+        a.add(1);
+
+        b.add(3);
+        b.add(3);
+        b.add(3);
+        b.add(4);
+
+        String result = Result.twoArrays(k, a, b);
+
+        assertThat(result).isEqualTo("NO");
+    }
 }
 
-/*
-3 10        A[] and B[] size n = 3, k = 10
-2 1 3       A = [2, 1, 3]
-7 8 9       B = [7, 8, 9]
-
-A' = [1,2,3]
-B'
-
- */
