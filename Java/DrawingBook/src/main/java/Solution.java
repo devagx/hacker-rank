545ffb6cd56cc4a288fc1d316e3d10a626985593
Alun Groome <alungroome@hotmail.com>
2023-02-06 14:52:21 +0000

    Created base project for DrawingBook.

---
+++ Java/DrawingBook/src/main/java/Solution.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        //int result = Result.pageCount(n, p);
+
+        //System.out.println(String.valueOf(result));
+
+        bufferedReader.close();
+    }
+}
