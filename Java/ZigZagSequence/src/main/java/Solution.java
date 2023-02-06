319a4c9c9d418401111a5d78e6522f370761b7ab
Alun Groome <alungroome@hotmail.com>
2023-02-06 10:46:00 +0000

    Created ZigZagSequence base project.

---
+++ Java/ZigZagSequence/src/main/java/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner kb = new Scanner(System.in);
+        int test_cases = kb.nextInt();
+        for (int cs = 1; cs <= test_cases; cs++) {
+            int n = kb.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = kb.nextInt();
+            }
+            //findZigZagSequence(a, n);
+        }
+    }
+}
