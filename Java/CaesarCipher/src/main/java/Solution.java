98da158091eba3a198d4d082d557d47af8a4a38d
Alun Groome <alungroome@hotmail.com>
2023-02-08 11:48:36 +0000

    Created CaesarCipher base project.

---
+++ Java/CaesarCipher/src/main/java/Solution.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        //String result = Result.caesarCipher(s, k);
+
+        //System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
