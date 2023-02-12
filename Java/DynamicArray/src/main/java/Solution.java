9db1fa5642a21b6b8b751eab420e1fa0b1e5e93b
Alun Groome <alungroome@hotmail.com>
2023-02-12 19:06:36 +0000

    Created base project for DynamicArray

---
+++ Java/DynamicArray/src/main/java/Solution.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, q).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+//        List<Integer> result = Result.dynamicArray(n, queries);
+//
+//        bufferedWriter.write(
+//                result.stream()
+//                        .map(Object::toString)
+//                        .collect(joining("\n"))
+//                        + "\n"
+//        );
+
+        bufferedReader.close();
+
+    }
+}
