a4ed8ba6f53473df7c6c76eebf1c8808b3a671ef
Alun Groome <alungroome@hotmail.com>
2023-02-14 15:18:20 +0000

    Created based project for GridChallenge

---
+++ Java/GridChallenge/src/main/java/Solution.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+                            try {
+                                return bufferedReader.readLine();
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        })
+                        .collect(toList());
+
+                //String result = Result.gridChallenge(grid);
+
+                // System.out.println(result);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        bufferedReader.close();
+    }
+}
